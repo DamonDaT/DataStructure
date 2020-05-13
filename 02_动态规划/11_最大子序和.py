991bb814de4bd4b25a13f0b2782e3c0d6382ea9b
805174018@qq.com <805174018@qq.com>
2020-05-13 16:54:30 +0800

    add 最大子序和 to 动态规划

---
+++ 02_动态规划/11_最大子序和.py
@@ -0,0 +1,32 @@
+'''
+【动态规划】 题目: 给定一个数组，求该数组的最大子序和
+'''
+'''
+思路: 
+cur_max: 以第i个数字作为最后一个数字时，得到的最大子序和
+last_max: 当前最大的子序和
+max_res: 存放结果
+'''
+
+def maxsubArray(nums):
+    # 判断长度
+    if len(nums) == 1:
+        return nums[0]
+    # max_res 存放最后的结果
+    max_res = nums[0]
+    # cur_max 存放到第i个数字的最大的值
+    cur_max = last_max = nums[0]
+    for i in range(1, len(nums)):
+        # if else 语句确定 cur_max 的值
+        if last_max + nums[i] < nums[i]:
+            cur_max = nums[i]
+        else:
+            cur_max = last_max + nums[i]
+        # 更新结果
+        if cur_max > max_res:
+            max_res = cur_max
+        # 更新当前的最大子序和
+        last_max = cur_max
+    return max_res
+
+print(maxsubArray([1,2,3,-1,4]))
